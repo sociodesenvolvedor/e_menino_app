97abb2d33fe90ab6161c7be060f8537a47753036
Raphael Morales <raphael.morales@outlook.com>
2019-05-09 20:44:58 -0300

    push notification

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import CodePush from 'react-native-code-push';
+import OneSignal from 'react-native-onesignal';
 
 import '~/config/ReactotronConfig';
 
@@ -9,7 +10,26 @@ import store from './store';
 import Routes from '~/routes';
 
 class App extends Component {
-  componentWillUnmount() {}
+  constructor(properties) {
+    super(properties);
+    OneSignal.init('6fb2a5b6-ae5c-46ea-ae76-461cc97c0f58');
+
+    OneSignal.addEventListener('received', this.onReceived);
+    OneSignal.addEventListener('opened', this.onOpened);
+    OneSignal.addEventListener('ids', this.onIds);
+  }
+
+  componentWillUnmount() {
+    OneSignal.removeEventListener('received', this.onReceived);
+    OneSignal.removeEventListener('opened', this.onOpened);
+    OneSignal.removeEventListener('ids', this.onIds);
+  }
+
+  onReceived(notification) {}
+
+  onOpened(openResult) {}
+
+  onIds(device) {}
 
   render() {
     return (
