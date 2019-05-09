45555284e4d9796ae7dffbedc48fa95fd66adb7d
Raphael Morales <raphael.morales@outlook.com>
2019-05-09 15:36:14 -0300

    install fonts

--- src/routes.js
+++ src/routes.js
@@ -1,17 +1,46 @@
+import React from 'react';
 import {
   createAppContainer,
   createBottomTabNavigator,
   createSwitchNavigator,
 } from 'react-navigation';
 
+/* Imports */
+import Ionicons from 'react-native-vector-icons/FontAwesome';
+
+/* Rotas */
 import Main from '~/pages/Main';
 import Instagram from '~/pages/Instagram';
 
-// const Routes = createAppContainer(createSwitchNavigator({ Main }));
+const Routes = createBottomTabNavigator(
+  {
+    Loja: Main,
+    Instagram,
+  },
+  {
+    defaultNavigationOptions: ({ navigation }) => ({
+      tabBarIcon: ({ focused, horizontal, tintColor }) => {
+        const { routeName } = navigation.state;
+        const IconComponent = Ionicons;
+        let iconName;
+        if (routeName === 'Loja') {
+          iconName = 'shopping-bag';
+          // Sometimes we want to add badges to some icons.
+          // You can check the implementation below.
+          // IconComponent = HomeIconWithBadge;
+        } else if (routeName === 'Instagram') {
+          iconName = 'instagram';
+        }
 
-const Routes = createBottomTabNavigator({
-  Loja: Main,
-  Instagram,
-});
+        // You can return any component that you like here!
+        return <IconComponent name={iconName} size={18} color={tintColor} />;
+      },
+    }),
+    tabBarOptions: {
+      activeTintColor: 'tomato',
+      inactiveTintColor: 'gray',
+    },
+  },
+);
 
 export default createAppContainer(Routes);
